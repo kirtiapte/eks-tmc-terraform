912fd0e2bfccc5b285dca48163aa13acbb4b13db
Kirti Apte <kirtiapte@yahoo.com>
2023-05-02 17:09:08 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,13 +8,3 @@ variable "vmw_api_token" {
   sensitive   = true
   description = "TMC API Token"
 }
-
-variable "account_id" {
-  type        = string
-  description = "Account ID for AWS"
-}
-
-variable "arn" {
-  type        = string
-  description = "IAM role ARN for AWS"
-}
